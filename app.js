8baa6b7633ea263268918c5db737d2dab685fdfc
Jisnakp <jisnab01@gmail.com>
2024-08-11 17:14:50 +0530

    project completed

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-const express=require("express")
-const mongoose=require("mongoose")
-const bcrypt=require("bcrypt")
-const jwt=require("jsonwebtoken")
-const cors=require("cors")
+const express = require("express")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const cors = require("cors")
 const loginModel = require("./admin")
-const app=express()
+const PeopleModel = require("./Missingpeople")
+
+const app = express()
 app.use(cors())
 app.use(express.json())
 mongoose.connect("mongodb+srv://jisnab01:jisnab01@cluster0.tnvz6eq.mongodb.net/rescuedb?retryWrites=true&w=majority&appName=Cluster0")
 
-app.get("/test",(req,res)=>{res.json({"status":"success"})})
+app.get("/test", (req, res) => { res.json({ "status": "success" }) })
 
-app.post("/adminSignin",(req,res)=>{
+app.post("/adminSignin", (req, res) => {
 
-    let input=req.body
-    let result=loginModel.find({username:input.username}).then(
-        (response)=>{
-            if (response.length>0) {
+    let input = req.body
+    let result = loginModel.find({ username: input.username }).then(
+        (response) => {
+            if (response.length > 0) {
 
-                const validator=bcrypt.compareSync(input.password,response[0].password)
+                const validator = bcrypt.compareSync(input.password, response[0].password)
                 if (validator) {
-                    
-                    jwt.sign({email:input.username},"rescue-app",{expiresIn:"3d"},
 
-                        (error,token)=>{
+                    jwt.sign({ email: input.username }, "rescue-app", { expiresIn: "3d" },
+
+                        (error, token) => {
                             if (error) {
 
-                                res.json({"status":"Invalid Authentication"})
-                                
+                                res.json({ "status": "Invalid Authentication" })
+
                             } else {
-                                res.json({"status":"success","token":token})
-                                
+                                res.json({ "status": "success", "token": token })
+
                             }
                         }
                     )
@@ -38,32 +40,54 @@ app.post("/adminSignin",(req,res)=>{
 
 
                 } else {
-                    res.json({"status":"Invalid Password"})
+                    res.json({ "status": "Invalid Password" })
                 }
-                
+
             } else {
-                res.json({"status":"Invalid Username"})
-                
+                res.json({ "status": "Invalid Username" })
+
             }
         }
     ).catch()
 })
 
 
-app.post("/adminSignUp",(req,res)=>{
+app.post("/adminSignUp", (req, res) => {
 
-      let input=req.body
-      let hashedPassword=bcrypt.hashSync(input.password,10)
-      //console.log(hashedPassword)
-      input.password=hashedPassword
-      console.log(input)
-      let result=new loginModel(input)
-      result.save()
-      res.json({ "status": "success"})
+    let input = req.body
+    let hashedPassword = bcrypt.hashSync(input.password, 10)
+    //console.log(hashedPassword)
+    input.password = hashedPassword
+    console.log(input)
+    let result = new loginModel(input)
+    result.save()
+    res.json({ "status": "success" })
+
+})
+
+app.post("/AddMissingpeople", (req,res) => {
+    let input = req.body
+    let token = req.headers.token
+    jwt.verify(token, "rescue-app",
+        (error, decoded) => {
+            if (decoded && decoded.email) {
+
+                let result = new PeopleModel(input)
+                result.save()
+                res.json({ "status": "success" })
+
+            } else {
+                res.json({ "status": "Failed to register" })
+
+
+            }
+        }
+    )
+
 
 })
 
 
-app.listen(8082,()=>{
+app.listen(8082, () => {
     console.log("server started")
 })
